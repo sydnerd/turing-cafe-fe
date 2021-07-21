89fe690862095ab012615078b1ea71bec6486ef6
Sydney Bear <sbear3915@gmail.com>
2021-07-21 10:24:39 -0600

    Add name input for form

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,8 +5,24 @@ class Form extends Component {
   constructor() {
     super();
     this.state = {
-
+      name = '',
+      date = '',
+      time = '',
+      number = 0
     }
+  }
+
+  render() {
+    return (
+      <form>
+        <input 
+          type='text'
+          placeholder='Name'
+          name='name'
+          value={this.state.name}
+        />
+      </form>
+    )
   }
 }
 
