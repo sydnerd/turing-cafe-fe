5926f15a6f7564020afeec0f71c554e25bb2aeb4
Sydney Bear <sbear3915@gmail.com>
2021-07-21 10:11:16 -0600

    Reservations are displaying on page

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -6,10 +6,19 @@ const Reservations = ({resys}) => {
   const resyCards = resys.map(res => {
     return (
       <Card
-        
+        name = {res.name}
+        date = {res.date}
+        time = {res.time}
+        number = {res.number}
       />
     )
   })
+
+  return (
+    <div className = 'res-container'>
+      {resyCards}
+    </div>
+  )
 }
 
 export default Reservations;
